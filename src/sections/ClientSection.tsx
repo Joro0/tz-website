005ae00b839587311bafd1e71a4c6c2991ce9ae5
joro <jose.alonsozv12@gmail.com>
2025-05-11 14:57:35 -0500

    ClientSection: added pattern

--- src/sections/ClientSection.tsx
+++ src/sections/ClientSection.tsx
@@ -7,48 +7,55 @@ import LogoUnicon from "../assets/clients/unicon.svg?react";
 import LogoYura from "../assets/clients/yura.svg?react";
 import LogoFdaniel from "../assets/clients/fdaniel.svg?react";
 import CarouselInfinite from "../components/CarouselInfinite";
+import LinePattern from "../assets/patterns/line_pattern.svg?react";
 
 function ClientSection() {
+  const colorLogo = "text-image"; // Define the color for the logos
   const data = [
     {
       name: "Southern Peru",
-      svgLogo: <LogoSouthern className="text-main" />,
+      svgLogo: <LogoSouthern className={colorLogo} />,
     },
     {
       name: "Anglo América",
-      svgLogo: <LogoAnglo className="text-main" />,
+      svgLogo: <LogoAnglo className={colorLogo} />,
     },
     {
       name: "Cosapi",
-      svgLogo: <LogoCosapi className="text-main" />,
+      svgLogo: <LogoCosapi className={colorLogo} />,
     },
     {
       name: "Graña y Montero",
-      svgLogo: <LogoGym className="text-main" />,
+      svgLogo: <LogoGym className={colorLogo} />,
     },
     {
       name: "Minsur",
-      svgLogo: <LogoMinsur className="text-main" />,
+      svgLogo: <LogoMinsur className={colorLogo} />,
     },
     {
       name: "Unicon",
-      svgLogo: <LogoUnicon className="text-main" />,
+      svgLogo: <LogoUnicon className={colorLogo} />,
     },
     {
       name: "Yura",
-      svgLogo: <LogoYura className="text-main" />,
+      svgLogo: <LogoYura className={colorLogo} />,
     },
     {
       name: "Fluor Daniel",
-      svgLogo: <LogoFdaniel className="text-main" />,
+      svgLogo: <LogoFdaniel className={colorLogo} />,
     },
   ];
 
   return (
-    <section id="clientes" className="w-full">
-      <div></div>
-      <h3 className="text-main text-center">CLIENTS</h3>
-      <CarouselInfinite data={data} wLogo={15} gLogo={10} rLogo={2} />
+    <section
+      id="clientes"
+      className="relative w-full bg-secondary py-3 overflow-hidden"
+    >
+      <div className="absolute inset-0 z-0 left-0">
+        <LinePattern className="w-1/2 opacity-15 text-image" />
+      </div>
+      <h3 className="text-highlight text-center mt-0">CLIENTES</h3>
+      <CarouselInfinite data={data} wLogo={14} gLogo={8} rLogo={2} />
     </section>
   );
 }
