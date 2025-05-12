43056eb46985380e8053df48387e1dff3e1934c0
joro <jose.alonsozv12@gmail.com>
2025-05-11 21:56:29 -0500

    Header: added color transition

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,18 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LogoTz from "../assets/logotz.svg?react";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-background-0 shadow-md z-50 border-b-1 border-image/40">
+    <header
+      className={`fixed top-0 left-0 right-0 z-50  border-b-1  transition-colors duration-500 ${
+        isScrolled
+          ? "bg-background border-main/20"
+          : "bg-background-0 border-image/40"
+      }`}
+    >
       <div className="container mx-auto flex items-center justify-between">
         {/* Left: Logo */}
-        <div className="flex flex-row py-2 px-4 gap-2 border-r-1 border-image/40">
+        <div
+          className={`flex flex-row py-2 px-4 gap-2 border-r-1 ${
+            isScrolled ? " border-main/20" : "border-image/40"
+          }`}
+        >
           <LogoTz className="w-9" />
           <div>
-            <p className="text-sm text-image">TRANSPORTES</p>
-            <p className="text-sm text-image">ZUÑIGA S.R.L.</p>
+            <p
+              className={`text-sm font-semibold transition-colors duration-500 ${
+                isScrolled ? "text-main" : "text-image"
+              }`}
+            >
+              TRANSPORTES
+            </p>
+            <p
+              className={`text-sm font-semibold transition-colors duration-500 ${
+                isScrolled ? "text-main" : "text-image"
+              }`}
+            >
+              ZUÑIGA S.R.L.
+            </p>
           </div>
         </div>
 
